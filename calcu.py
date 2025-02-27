ff2a3e174b966128e5de2d04452eeea46d8cf154
Joseruel111 <macatol.313593@laspinas.sti.edu.ph>
2025-02-27 13:31:02 +0800

    calcu.py

--- calcu.py
+++ calcu.py
@@ -1,124 +1,110 @@
-#Token types
-#
-#EOF(end-of-file)token is used to indicate that
-#there is no more input left for lexical analysis
-INTEGER,PLUS,EOF = 'INTEGER','PLUS','EOF'
-
-class Token(object):
-    def __init__(self,type,value):
-        #token type: INTEGER,PLUS, or EOF
-        self.type = type
-        #token value: 0,1,2,3,4,5,6,7,8,9,'+', or None
+class Token:
+    def __init__(self, type_, value):
+        self.type = type_
         self.value = value
-        
-        def __str__(self):
-            """String representation of the class instance.
-            
-            Examples:
-            
-            Token(INTEGER, 3)
-            Token(PLUS '+')
-            
-            """
-            
-            return 'Token({type},{value})'.format(
-                type=self.type,
-                value=repr(self.value)
-                )
-                
-        def __repr__(self):
-            return self.__str__()
-                    
-class Interpreter(object):
-    def __init__(self,text):
-        #client string input,e.g "3+5"
-        self.text=text
-        #self.pos is an index into self.text
+
+    def __str__(self):
+        return f"Token({self.type}, {self.value})"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+# Token types
+INTEGER, PLUS, MINUS, EOF = 'INTEGER', 'PLUS', 'MINUS', 'EOF'
+
+
+class Lexer:
+    def __init__(self, text):
+        self.text = text
         self.pos = 0
-        #current token instance
-        self.current_token = None
-        
-        def error(self):
-            raise Exception('Error parsing input')
-            
-            def get_next_token(self):
-                """Lexical analyzer(also known as scanner or tokenizer)
-                
-                This method is responsible for breaking a sentence
-                apart into tokens. One token at a time.
-                """
-                
-                text = self.text
-                
-                # is self.pos index past the end of the self.text?
-                #if so, then EOF token because there is no more
-                #input left to conver into tokens
-                if self.pos > len(text) - 1:
-                    return Token(EOF,None)
-                    
-                    #get a character at the postion self.pos and decide
-                    #what token to create based on the single character
-                    current_char = text[self.pos]
-                    
-                    #if the character is a digit then convert it to
-                    #integer, create an INTEGER token, increment self.pos
-                    #index to point to the next character after the digit,
-                    #and return the INTEGER token
-                    if current_char.isDigit():
-                        token = Token(INTEGER,int(current_char))
-                        self.pos += 1
-                        return token
-                        
-                        if current_char == '+':
-                            token = Token(PLUS,current_char)
-                            self.pos += 1
-                            return token
-                            
-                            self.error()
-                def eat(self,token_type):
-                    #compare the current token type with the passed token
-                    #type and if they match then "eat" the current token
-                    #and assign the next token to the self.current_token
-                    #otherwise raise an exception.
-                    if self.current_token.type == token_type:
-                        self.current_token = self.get_next_token()
-                    else:
-                        self.error()
-                        
-                def expr(self):   
-                    """expre -> INTEGER PLUS INTEGER"""
-                    #set current token to the first token taken from the input
-                    self.current_token = self.get_next_token()
-                    #left = self.current_token
-                    self.eat(INTEGER)
-                    #we expect the current token to be a '+' token
-                    op = self.current_token
-                    self.eat(PLUS)
-                    #we expect the current oken to be a single-digit integer
-                    right = self.current_token
-                    self.eat(INTEGER)
-                    #after the above call the self.current_token is set to
-                    #EOF token
-                    
-                    #at this point INTEGER PLUS INTEGER sequence of tokens
-                    # has been successfully found and the method can just
-                    #return the result of adding two integers, thus
-                    #effectively interpreting input
-                    result = left.value + right.value
-                    return result
-                    
-            def main():
-                while True:
-                    try:
-                        #To run under Python3 replace 'raw_input' call
-                        #with 'input'
-                        text = raw_input('calc>')
-                    except EOFError:
-                            break
-                    if not text:
-                            continue
-                    interpreter = Interpreter(text)
-                    result = interpreter.expr()
-                        
-            if __name__=='__main__':
-                main()
+        self.current_char = self.text[self.pos]
+
+    def error(self):
+        raise Exception('Invalid character')
+
+    def advance(self):
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None  # End of input
+        else:
+            self.current_char = self.text[self.pos]
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def integer(self):
+        result = ""
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
+
+    def get_next_token(self):
+        while self.current_char is not None:
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+            if self.current_char.isdigit():
+                return Token(INTEGER, self.integer())
+            if self.current_char == '+':
+                self.advance()
+                return Token(PLUS, '+')
+            if self.current_char == '-':
+                self.advance()
+                return Token(MINUS, '-')
+            self.error()
+        return Token(EOF, None)
+
+
+class Interpreter:
+    def __init__(self, text):
+        self.lexer = Lexer(text)
+        self.current_token = self.lexer.get_next_token()
+
+    def error(self):
+        raise Exception('Invalid syntax')
+
+    def eat(self, token_type):
+        if self.current_token.type == token_type:
+            self.current_token = self.lexer.get_next_token()
+        else:
+            self.error()
+
+    def expr(self):
+        # Expect an INTEGER
+        left = self.current_token
+        self.eat(INTEGER)
+
+        # Expect either PLUS or MINUS
+        op = self.current_token
+        if op.type == PLUS:
+            self.eat(PLUS)
+        elif op.type == MINUS:
+            self.eat(MINUS)
+
+        # Expect another INTEGER
+        right = self.current_token
+        self.eat(INTEGER)
+
+        # Perform the calculation and return result
+        if op.type == PLUS:
+            return left.value + right.value
+        else:
+            return left.value - right.value
+
+
+def main():
+    while True:
+        try:
+            text = input("calc> ")
+            interpreter = Interpreter(text)
+            result = interpreter.expr()
+            print(result)
+        except Exception as e:
+            print(e)
+
+
+if __name__ == '__main__':
+    main()
